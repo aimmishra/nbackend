111de0dd0dc2dc92478c2c002a18272224cb59a1
lakshya mishra <missioncow1@gmail.com>
2024-05-02 16:00:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Chai aur code");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) =>{
+    res.send('lakshyadotcom')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
